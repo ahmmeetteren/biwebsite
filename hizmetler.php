8dc6222625da6b9e24f2eb078989792ea292d84b
Ahmet Eren Yılmaz <ahmtylmmz186@gmail.com>
2022-05-17 20:27:23 +0300

    UPDATE

--- hizmetler.php
+++ hizmetler.php
@@ -28,7 +28,7 @@ $hizmetMaliyet2 = mysqli_query($baglanti, "SELECT hizmetler.hizmet_ad, hizmetler
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/f6190b6e7c.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Anasayfa</title>
+    <title>Hizmetler</title>
 </head>
 
 <body>
